48856ce3c952064c67098cf2ffc3b311d461959c
“nimritee” <nimritee97@gmail.com>
2021-10-15 19:11:11 +0200

    data type conversion

---
+++ Basics/data_conversion.py
@@ -0,0 +1,18 @@
+#From Tuple to List
+
+mytuple = (1,2,3)
+mylist = list(mytuple)
+print(mylist)
+
+# From list to Tuple 
+
+mylist1 =[4,5,6]
+mytuple1 = tuple(mylist1)
+print(mytuple1)
+
+
+#From List to Dictionary
+
+mylist2 = [["name","Nimirtee"],["name1","Divya"]]
+mydict = dict(mylist2)
+print(mydict)
