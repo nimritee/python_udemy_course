92ce1c1b4e032cde0d13817777f7639f86fd9591
“nimritee” <nimritee97@gmail.com>
2021-11-02 12:03:29 +0100

    imporve the code flow

--- MLDM Assignment/userClass.py
+++ MLDM Assignment/userClass.py
@@ -14,24 +14,32 @@ class User():
     def setFirstname(self,newname):
         self.firstname = newname
     
-    def getAge(self):
+    def validDate(self):
+        isValid = True
         month,day,year = self.dateOfBirth.split('-')
         try:
             dt.datetime(int(year), int(month), int(day))
         except ValueError:
-            return "Please provide a valid Date of Birth, in the proper format"
+            isValid = False
+        return isValid
+
+    def getAge(self):
         dob =  datetime.strptime(self.dateOfBirth, '%m-%d-%Y').date()
         today = date.today()
         years = today.year - dob.year - ((today.month,today.day) < (dob.month,dob.day))
         return ("The age of the user is: {} Years".format(years))
 
+
 username = input("Hello, Please enter your first name: ")
 dob = input("Hi {}, please enter your birthdate in 'MM-DD-YYYY' format.\n".format(username))
 myuser= User(username,dob)
 
-print('The first name of the user is: ', myuser.getFirstname())
-newname = input('Enter the new user name: ')
-changeUsername = myuser.setFirstname(newname)
-print('The first name of the user has been updated to:', myuser.getFirstname())
+if myuser.validDate():
+    print('The first name of the user is: ', myuser.getFirstname())
+    newname = input('Enter the new user name: ')
+    changeUsername = myuser.setFirstname(newname)
+    print('The first name of the user has been updated to:', myuser.getFirstname())
+    print(myuser.getAge())
 
-print(myuser.getAge())
+else:
+    print("Please provide a valid Date of Birth, in the proper format")
