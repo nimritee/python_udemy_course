f180e6e37ed7a85dabde6c84cebd72c20ff9a731
“nimritee” <nimritee97@gmail.com>
2021-11-30 23:28:52 +0100

    change

--- DataScience/visualization3.py
+++ DataScience/visualization3.py
@@ -6,13 +6,12 @@ import matplotlib.pyplot as plt
 # Read the csv data
 data = pd.read_csv('data3.csv')
 
-# Create range of height, for plotting the data
-data['bar_bins'] = pd.cut(data.height,bins=[50,75,100,125,150,175,200],labels=['50-75','76-100','101-125','126-150','151-175','176-200'])
-bar_data= data['bar_bins'].value_counts().reset_index()
+height_data = data['height']
+bins = [50,60,70,80,90,100,110,120,130,140,150,160,170,180]
 
 # Plot the Graph
-plt.bar(bar_data['index'], bar_data['bar_bins'])
-plt.ylabel('Count')
-plt.xlabel('Height')
-plt.title('Barplot: Height of people in !Kung San')
+plt.hist(height_data, bins=bins, edgecolor='black')
+plt.title('Histogram: Height of people in !Kung San')
+plt.xlabel("Height")
+plt.ylabel("Number of people")
 plt.show()
