2b5731d0cfd4aaeed5b8207c43ed50cf45771302
“nimritee” <nimritee97@gmail.com>
2021-12-03 19:32:43 +0100

    exponential distributiom

---
+++ DataScience/exponential_distribution.py
@@ -0,0 +1,17 @@
+# Exponential Distribution 
+# Parameters
+    # scale: inverse rate of (lam) deafult 1.0
+    # size: the shape of the returned array
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Plot the graph
+fig, ax = plt.subplots(nrows=4, ncols=4, figsize=(20,25))
+size = 1000
+for i,ax in zip(range(16),ax.ravel()):
+    sns.distplot(random.exponential(scale=i,size=1000), hist=False, ax=ax) 
+    size = size + (i*100)
+    ax.set(xlabel='scale ={} and size ={}'.format(i,size))
+plt.show()
