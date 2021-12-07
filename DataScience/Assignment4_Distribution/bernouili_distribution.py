f174fbb026752659b01c77557d676599947792c0
“nimritee” <nimritee97@gmail.com>
2021-12-07 13:54:59 +0100

    bernoulli

--- DataScience/Assignment4_Distribution/bernouili_distribution.py
+++ DataScience/Assignment4_Distribution/bernouili_distribution.py
@@ -1,21 +1,21 @@
 # Bernoulli Distribution
-# Parameters:
-    # size : 
+# The bernoulli distribution is a discrete distribution that is used when a random experiment is performed and only two results are obtained,
+# such as good-bad, positive-negative, success-failure.
 
 from scipy.stats import bernoulli
 import matplotlib.pyplot as plt
 import seaborn as sb
 
-# Plot the graph
-fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10,15))
+x=[0,1]
+p=0.7
 
-size = 1500
-p=0.2
-for i,ax in zip(range(4),ax.ravel()):
-    data_bern = bernoulli.rvs(size=size,p=p)
-    sb.distplot(data_bern,ax=ax)
-    ax.set(xlabel='size ={} and p ={}'.format(size,p))
-    size = size + (i*100)
-    p = p + 0.1
-    fig.suptitle('Bernoulli Distribution',fontweight ="bold")
+fig, (ax1, ax2) = plt.subplots(2)
+ax1.bar(x,bernoulli.pmf(x,p),width=0.1,color=['r','b'])
+ax1.set_title('Probability Mass Function for Bernoulli Distribution')
+
+ax2.bar(x,bernoulli.cdf(x,p),width=0.1,color=['r','b'])
+ax2.set_title('Cumulative Density  Function for Bernoulli Distribution')
+
+fig.text(0.5, 0.04, 'Data Points', ha='center', va='center')
+fig.text(0.06, 0.5, 'Probability', ha='center', va='center', rotation='vertical')
 plt.show()
